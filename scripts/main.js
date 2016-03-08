06541212035daa46a1c263a8fa33de339d4e727f
Mory Friedman <friedmanmory2@gmail.com>
2016-03-07 22:24:18 -0600

    Paint Calculator

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+var width = prompt('What is the width of the room?');
+var length = prompt('What is the length of the room?');
+var answer = parseFloat(length*width);
+var area = Math.ceil(length*width/350);
+
+document.write('<div>How many square feet do you need to paint? '+answer+'</div>')
+document.write('You will need to purchase '+area+' '+'gallons of paint to cover '+answer+ ' '+ 'feet.');
+
+
+
