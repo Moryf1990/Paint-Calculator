a0de5310e79cd0cccf21c91c4bd226f0b3b99acc
Mory Friedman <friedmanmory2@gmail.com>
2016-03-13 10:09:09 -0500

    Fixing parseFloat

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 var width = prompt('What is the width of the room?');
 var length = prompt('What is the length of the room?');
-var answer = parseFloat(length*width);
+var answer = parseFloat(length)*parseFloat(width);
 var area = Math.ceil(length*width/350);
 
 document.write('<div>How many square feet do you need to paint? '+answer+'</div>')
